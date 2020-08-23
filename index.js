13f5e89c903e4b64a5ac815244a0a0a8be551ea2
Tanmoy Krishna Das <tanmoykrishnadas@gmail.com>
2020-08-23 17:14:05 +0600

    Add request body to response

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require('express')
+var bodyParser = require('body-parser')
+
 const app = express()
 const port = process.env.PORT || 3000
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.all(/.*/, (req, res) => {
     setTimeout(()=>{
         let response = JSON.stringify({
-            url : req.url
+            url : req.url,
+            body: `${JSON.stringify(req.body, null, 2)}`
         });
         res.send(response);
     }, 1000);
 
-    console.log(req);
+    console.log(req.body);
     console.log("\n\n\n");
 })
 
